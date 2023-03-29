23211adb45abdfd7e08a3ddf0ee210e958603505
SHERLIN LIDIYA A <128035801+SHERLINLIDIYA22@users.noreply.github.com>
2023-03-29 11:44:46 +0530

    Add files via upload

---
+++ React-Day-2/1.ClassWork/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Footer from './ClassWork2/Footer';
+import Navapp from './ClassWork2/Navapp';
+import Image from './ClassWork2/Image';
+function App() {
+  return (
+    <div>
+      <Navapp></Navapp>
+      <Image/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
